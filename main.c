26aecee861ed360ff94d2374e26883a927400440
Gioxgio <fantastici02@hotmail.it>
2025-11-08 11:23:24 +0100

    Add support to Enter and Ctrl+c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
@@ -10,112 +11,120 @@
 struct termios start_raw_mode(int);
 int wherex(int);
 void gotoxy(int, int);
+bool should_quit(char *);
 void redraw_line(char **, int, int, int);
 
 // TODO
-// Add support to Ctrl-C
-// Add support to Enter
 // Add support for overflowing lists
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Missing parameters!\n");
-        exit(EXIT_FAILURE);
-    }
+  if (argc < 2) {
+    printf("Missing parameters!\n");
+    exit(EXIT_FAILURE);
+  }
 
-    char c;
-    int lower_bound, pos, upper_bound;
+  char c;
+  int lower_bound, pos, upper_bound;
 
-    // init & start termios
-    int fd = STDIN_FILENO;
-    struct termios bkp_term;
-    bkp_term = start_raw_mode(fd);
+  // init & start termios
+  int fd = STDIN_FILENO;
+  struct termios bkp_term;
+  bkp_term = start_raw_mode(fd);
 
-    // init & start menu
-    lower_bound = wherex(fd);
-    pos = lower_bound;
-    upper_bound = lower_bound + argc - 2;
-    for (int i = 1; i < argc; i++) {
-        printf("%s\n\r", argv[i]);
-    }
-    redraw_line(argv + 1, lower_bound, pos, upper_bound);
+  // init & start menu
+  lower_bound = wherex(fd);
+  pos = lower_bound;
+  upper_bound = lower_bound + argc - 2;
+  for (int i = 1; i < argc; i++) {
+    printf("%s\n\r", argv[i]);
+  }
+  redraw_line(argv + 1, lower_bound, pos, upper_bound);
 
-    while (read(STDIN_FILENO, &c, 1) == 1 && (c != 'q' && c != '\n')) {
-        if (c == '[') {
-            read(fd, &c, 1);
-            switch (c - 'A') {
-            case 0: {
-                if (pos > lower_bound) {
-                    pos--;
-                    redraw_line(argv + 1, lower_bound, pos, upper_bound);
-                }
-                break;
-            }
-            case 1: {
-                if (pos < upper_bound) {
-                    pos++;
-                    redraw_line(argv + 1, lower_bound, pos, upper_bound);
-                }
-                break;
-            }
-            default: {
-                break;
-            }
-            }
+  while (read(STDIN_FILENO, &c, 1) == 1 && should_quit(&c)) {
+    if (c == '[') {
+      read(fd, &c, 1);
+      switch (c - 'A') {
+      case 0: {
+        if (pos > lower_bound) {
+          pos--;
+          redraw_line(argv + 1, lower_bound, pos, upper_bound);
         }
+        break;
+      }
+      case 1: {
+        if (pos < upper_bound) {
+          pos++;
+          redraw_line(argv + 1, lower_bound, pos, upper_bound);
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+      }
     }
+  }
 
-    // Stop raw mode
-    tcsetattr(fd, TCSANOW, &bkp_term);
+  // Stop raw mode
+  tcsetattr(fd, TCSANOW, &bkp_term);
 
-    return pos - lower_bound;
+  return pos - lower_bound;
 }
 
+// Utilities
 struct termios start_raw_mode(int fd) {
-    struct termios term, bkp_term;
+  struct termios term, bkp_term;
 
-    tcgetattr(fd, &term);
-    bkp_term = term;
-    cfmakeraw(&term);
-    tcsetattr(fd, TCSANOW, &term);
+  tcgetattr(fd, &term);
+  bkp_term = term;
+  cfmakeraw(&term);
+  tcsetattr(fd, TCSANOW, &term);
 
-    return bkp_term;
+  return bkp_term;
 }
 
 int wherex(int fd) {
-    int row;
-    // Request cursor position
-    write(fd, "\x1b[6n", 4);
+  int row;
+  // Request cursor position
+  write(fd, "\x1b[6n", 4);
 
-    // Read the response from stdin: ESC [ <row> ; <col> R
-    scanf("\x1b[%d;%*dR", &row);
-    return row;
+  // Read the response from stdin: ESC [ <row> ; <col> R
+  scanf("\x1b[%d;%*dR", &row);
+  return row;
 }
 
 void gotoxy(int x, int y) { printf("\033[%d;%dH", y, x); }
 
-void redraw_line(char **s, int lower_bound, int pos, int upper_bound) {
-    int i, len;
-    i = pos - lower_bound;
-    len = upper_bound - lower_bound;
-    if (i == 0) {
-        gotoxy(1, pos + 1);
-        printf(RESET "%s\n", s[i + 1]);
-    } else if (i == len) {
-        gotoxy(1, pos - 1);
-        printf(RESET "%s\n", s[i - 1]);
-    } else {
-        gotoxy(1, pos - 1);
-        printf(RESET "%s\n", s[i - 1]);
-        gotoxy(1, pos + 1);
-        printf(RESET "%s\n", s[i + 1]);
-    }
-
-    gotoxy(1, pos);
-    printf(MAGENTA "%s\n", s[i]);
-
-    gotoxy(1, upper_bound + 1);
-    printf(RESET "lower_bound: %d  pos: %d  upper_bound: %d\r\n", lower_bound,
-           pos, upper_bound);
+// Business
+bool should_quit(char *c) {
+  // Enter
+  return *c != '\r'
+         // Ctrl+c
+         && *c != 3;
 }
 
+void redraw_line(char **s, int lower_bound, int pos, int upper_bound) {
+  int i, len;
+  i = pos - lower_bound;
+  len = upper_bound - lower_bound;
+  if (i == 0) {
+    gotoxy(1, pos + 1);
+    printf(RESET "%s\n", s[i + 1]);
+  } else if (i == len) {
+    gotoxy(1, pos - 1);
+    printf(RESET "%s\n", s[i - 1]);
+  } else {
+    gotoxy(1, pos - 1);
+    printf(RESET "%s\n", s[i - 1]);
+    gotoxy(1, pos + 1);
+    printf(RESET "%s\n", s[i + 1]);
+  }
+
+  gotoxy(1, pos);
+  printf(MAGENTA "%s\n", s[i]);
+
+  gotoxy(1, upper_bound + 1);
+  printf(RESET
+         "lower_bound: %d  pos: %d  upper_bound: %d  return value: %d\r\n",
+         lower_bound, pos, upper_bound, pos - lower_bound);
+}
