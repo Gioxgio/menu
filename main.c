63bcd63517a03fa14000eca011ce756646e6842c
Gioxgio <fantastici02@hotmail.it>
2025-11-08 11:52:25 +0100

    Generic improvements

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define RESET "\033[0m"
 #define MAGENTA "\033[0;35m"
 
+enum arrow { UP = 0, DOWN = 1 };
+
 struct termios start_raw_mode(int);
-int wherex(int);
+int wherey(int);
 void gotoxy(int, int);
 bool should_quit(char *);
 void redraw_line(char **, int, int, int);
@@ -32,7 +33,7 @@ int main(int argc, char **argv) {
   bkp_term = start_raw_mode(fd);
 
   // init & start menu
-  lower_bound = wherex(fd);
+  lower_bound = wherey(fd);
   pos = lower_bound;
   upper_bound = lower_bound + argc - 2;
   for (int i = 1; i < argc; i++) {
@@ -44,14 +45,14 @@ int main(int argc, char **argv) {
     if (c == '[') {
       read(fd, &c, 1);
       switch (c - 'A') {
-      case 0: {
+      case UP: {
         if (pos > lower_bound) {
           pos--;
           redraw_line(argv + 1, lower_bound, pos, upper_bound);
         }
         break;
       }
-      case 1: {
+      case DOWN: {
         if (pos < upper_bound) {
           pos++;
           redraw_line(argv + 1, lower_bound, pos, upper_bound);
@@ -83,11 +84,10 @@ struct termios start_raw_mode(int fd) {
   return bkp_term;
 }
 
-int wherex(int fd) {
+int wherey(int fd) {
   int row;
   // Request cursor position
   write(fd, "\x1b[6n", 4);
-
   // Read the response from stdin: ESC [ <row> ; <col> R
   scanf("\x1b[%d;%*dR", &row);
   return row;
